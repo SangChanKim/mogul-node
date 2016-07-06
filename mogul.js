2d59655e774f4feaa351be31b195ffafdfca40b1
Sangchan Kim <sck970112@gmail.com>
2016-07-06 16:04:28 -0400

    Realtime reading from Firebase db

--- mogul.js
+++ mogul.js
@@ -1,4 +1,18 @@
-var Firebase = require("firebase");
+var firebase = require("firebase");
+var nessie = require('superagent');
+var nessieURL = 'http://api.reimaginebanking.com/atms?key=eddd389a9f4fbd8e6327b89923c3e4a1'
 
-var myFirebaseRef = new Firebase("https://mogul-6792d.firebaseio.com/");
+firebase.initializeApp({
+  serviceAccount: "./Mogul-a6b6dc3c36af.json",
+  databaseURL: "https://mogul-6792d.firebaseio.com"
+});
+
+var db = firebase.database();
+var requestRef = db.ref("/requests");
+
+
+// Listens for requests made by clients
+requestRef.on('child_added', function(snapshot, prevChildKey) {
+  console.log(snapshot.val());
+});
 
