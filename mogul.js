acb126b36ee6b552c281743afd5afe8f0c2509a0
Sangchan Kim <sck970112@gmail.com>
2016-07-07 14:22:54 -0400

    Working node backend

--- mogul.js
+++ mogul.js
@@ -25,12 +25,14 @@ requestRef.on('child_added', function(snapshot, prevChildKey) {
   	//processUpdateBalance(snapshot);
   } else if (type === 'requestBalance') {
   	processBalanceRequest(snapshot);
+  } else if (type === 'addTask') {
+  	processAddTask(snapshot);
   }
 });
 
 
 function processTransaction(snapshot) {
-
+	console.log("Process Transaction");
 	var payerUID = snapshot.val().payer;
 	var payeeUID = snapshot.val().payee;
 	var amount = snapshot.val().amount;
@@ -57,13 +59,36 @@ function processTransaction(snapshot) {
 }
 
 function processBalanceRequest(snapshot) {
+	console.log("Process Balance Request");
 	var requester = snapshot.val().requester;
 	db.ref('/users/' + requester).on('value', function(snapshot) {
 		var accountid = snapshot.val().accountid;
-		var url = nessieURL + "/accounts/" + accountid + "?key=" + nessieKey;
+		var url = nessieURL + '/accounts/' + accountid + '?key=' + nessieKey;
 		request.get(url).end(function(err, res) {
 			console.log(res.status + ": " + url);
 			db.ref("/users").child(requester).update({"balance": res.body.balance});
 		});
 	});
+}
+
+
+function processAddTask(snapshot) {
+	console.log("Process Add Task");
+	var taskname = snapshot.val().taskname;
+	var description = snapshot.val().description;
+	var owner = snapshot.val().owner;
+	var assignto = snapshot.val().assignto;
+	var reward = snapshot.val().reward;
+	var deadline = snapshot.val().deadline;
+	console.log(snapshot.val());
+	var r = db.ref('/users').child(assignto).child("tasks").push().set({"taskname": taskname, "description": description, "owner": owner, "completed": false, "reward": reward, "deadline": deadline});
+}
+
+function processTaskCompletion(snapshot) {
+	console.log("Process Task Completion");
+	var 
+}
+
+function processApproveCompletion(snapshot) {
+
 }
